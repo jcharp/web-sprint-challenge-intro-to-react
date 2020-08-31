a06cbca471b229ddddaeb6c2c992e5dc332f2e69
unknown <jackcharp69@gmail.com>
2020-08-30 20:44:16 -0400

    added my styled components but it did not work

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,14 +1,29 @@
 // Write your Character component here
 import React from 'react';
+import styled from 'styled-components';
+
+//writing out styled components part
+const WrapperDiv = styled.div`
+font-family; cursive;
+text-align; center
+border: 2px solid black;
+`;
+
 
 const Characters = ({name, gender, homeworld, img_url}) => {
     return (
-      <div>
+//wrapping the div in WrapperDiv
+    <WrapperDiv className="App">
+        <div>
         <h3>Name: {name}</h3>
         <h3>Gender: {gender} </h3>
         <h3>Home: {homeworld}</h3>
         <img url={img_url} />
-      </div>             
+      </div>  
+
+    </WrapperDiv>
+
+                 
     
     )
 }
